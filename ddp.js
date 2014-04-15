396b1e3ba7bbf53f769279bf1247ba5419b3943d
Paolo Scanferla <paolo.scanferla@gmail.com>
2014-04-15 16:40:42 +0200

    Added AMD and CommonJS support.

--- ddp.js
+++ ddp.js
@@ -1,4 +1,12 @@
-(function () {
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(factory);
+    } else if (typeof exports === "object") {
+        module.exports = factory();
+    } else {
+        root.DDP = factory();
+    }
+}(this, function () {
 
     "use strict";
 
@@ -201,10 +209,6 @@
 		this["_on_" + data.msg](data);
 	};
 
-    if (typeof module !== "undefined" && module.exports) {
-        module.exports = DDP;
-    } else {
-        window.DDP = DDP;
-    }
+	return DDP;
 
-})();
+}));
